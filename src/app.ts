143b8a404aea22ac34140a4dd2edd08823dbf58e
Andrew Weinstein <andrew@altvia.com>
2020-10-11 19:43:59 -0600

    class transformer

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
-import _ from 'lodash';
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
 
-console.log(_.shuffle([1, 2, 3]));
+import { Product } from './product.model';
+
+const p1 = new Product('Book', 12.99);
+// console.log(p1.getInformation());
+
+// what if this were untyped JSON returned from an API call?
+const products = [
+  { title: 'Carpet', price: 29.99 },
+  { title: 'Book', price: 10.99 },
+];
+
+// we could do it manually, like...
+const typedProducts = products.map((p) => new Product(p.title, p.price));
+// typedProducts.forEach((p) => console.log(p.getInformation()));
+
+// or we could use the class-transformer package (works for TS or vanilla JS)
+const transformed = plainToClass(Product, products);
+transformed.forEach((p) => console.log(p.getInformation()));
